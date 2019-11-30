d96332cf487922a15886fc7f65787c795911a077
Darren Cook <thelurkingdev@gmail.com>
2019-11-30 00:12:20 -0500

    Added WebRequestService. Created work GET request.

---
+++ Assets/Web Requests/WebRequestService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebRequestService : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(GetCoroutine("https://jsonplaceholder.typicode.com/todos/1"));
+    }
+
+    private IEnumerator GetCoroutine(string url)
+    {
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(url))
+        {
+            yield return unityWebRequest.SendWebRequest();
+
+            Debug.Log(unityWebRequest.downloadHandler.text);
+        }
+    }
+}
