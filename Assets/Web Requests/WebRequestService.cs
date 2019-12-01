dcb59d0baa0c57f9fdd5bca4fe1eda261246e7d5
Darren Cook <thelurkingdev@gmail.com>
2019-11-30 20:49:38 -0500

    Added WebRequests static class. Moved request logic to WebRequests.

--- Assets/Web Requests/WebRequestService.cs
+++ Assets/Web Requests/WebRequestService.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.Networking;
+﻿using UnityEngine;
 
 public class WebRequestService : MonoBehaviour
 {
@@ -9,45 +7,7 @@ public class WebRequestService : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(GetCoroutine("https://jsonplaceholder.typicode.com/todos/1"));
-        StartCoroutine(GetTextureCoroutine("https://via.placeholder.com/600/92c952"));
-    }
-
-    private IEnumerator GetCoroutine(string url)
-    {
-        using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(url))
-        {
-            yield return unityWebRequest.SendWebRequest();
-
-            if(unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
-            {
-                Debug.Log(unityWebRequest.error);                
-            }
-            else
-            {
-                Debug.Log(unityWebRequest.downloadHandler.text);
-            }
-        }
-    }
-
-    private IEnumerator GetTextureCoroutine(string url)
-    {
-        using (UnityWebRequest unityWebRequest = UnityWebRequestTexture.GetTexture(url))
-        {
-            yield return unityWebRequest.SendWebRequest();
-
-            if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
-            {
-                Debug.Log(unityWebRequest.error);
-            }
-            else
-            {
-                DownloadHandlerTexture downloadHandlerTexture = unityWebRequest.downloadHandler as DownloadHandlerTexture;
-                Sprite sprite = Sprite.Create(downloadHandlerTexture.texture, 
-                    new Rect(0, 0, downloadHandlerTexture.texture.width, downloadHandlerTexture.texture.height),
-                    new Vector2(0.5f, 0.5f));
-                _spriteRenderer.sprite = sprite;                
-            }
-        }        
+        StartCoroutine(WebRequests.GetCoroutine("https://jsonplaceholder.typicode.com/todos/1"));
+        StartCoroutine(WebRequests.GetTextureCoroutine("https://via.placeholder.com/600/92c952", _spriteRenderer));
     }
 }
---
+++ Assets/Web Requests/WebRequests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class WebRequests
+{
+
+    public static IEnumerator GetCoroutine(string url)
+    {
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Get(url))
+        {
+            yield return unityWebRequest.SendWebRequest();
+
+            if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                Debug.Log(unityWebRequest.error);
+            }
+            else
+            {
+                Debug.Log(unityWebRequest.downloadHandler.text);
+            }
+        }
+    }
+
+    public static IEnumerator GetTextureCoroutine(string url, SpriteRenderer spriteRenderer)
+    {
+        using (UnityWebRequest unityWebRequest = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return unityWebRequest.SendWebRequest();
+
+            if (unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                Debug.Log(unityWebRequest.error);
+            }
+            else
+            {
+                DownloadHandlerTexture downloadHandlerTexture = unityWebRequest.downloadHandler as DownloadHandlerTexture;
+                Sprite sprite = Sprite.Create(downloadHandlerTexture.texture,
+                    new Rect(0, 0, downloadHandlerTexture.texture.width, downloadHandlerTexture.texture.height),
+                    new Vector2(0.5f, 0.5f));
+                spriteRenderer.sprite = sprite;
+            }
+        }
+    }
+}
